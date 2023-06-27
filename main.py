49be78132ec0ddb2a85a1597d4c38ec86b9acfd2
alopacin <adlopacinski@gmail.com>
2023-06-27 17:54:43 +0200

    zapisanie pracy

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
 import json
 import os
+import datetime
+
+class WeatherForecast :
+    def __init__(self, latitude, longitude, date, temperature, windspeed):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.date = date
+        self.temperature = temperature
+        self.windspeed = windspeed
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __iter__(self):
+        yield self
+
 
 f = 'history.txt'
 if os.path.exists(f):
@@ -9,48 +28,49 @@ if os.path.exists(f):
             history_of_weather = json.load(file)
         except json.JSONDecodeError:
             history_of_weather = {}
+else:
+    history_of_weather = {}
+print(history_of_weather)
+tomorrow = datetime.date.today() + datetime.timedelta(days = 1)
+formatted_tomorrow = tomorrow.strftime('%Y-%m-%d')
+requested_date = input('Wpisz datę we formacie YYYY-mm-dd: ')
 
-class WeatherForecast :
-    def __init__(self, latitude, longitude, start_date, end_date, temperature, windspeed):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.start_date = start_date
-        self.end_date = end_date
-        self.temperature = temperature
-        self.windspeed = windspeed
+if requested_date == '' :
+    requested_date = formatted_tomorrow
 
-    def __setitem__(self, key, value):
-        self.key = value
+if requested_date in history_of_weather.keys():
+    latitude = history_of_weather[requested_date]['latitude']
+    longitude = history_of_weather[requested_date]['longitude']
+    temperature = history_of_weather[requested_date]['temperature']
+    windspeed = history_of_weather[requested_date]['windspeed']
+    weather = WeatherForecast(latitude, longitude, requested_date, temperature, windspeed)
+    print('ta data była juz sprawdzana')
+    exit()
 
-    def __getitem__(self, item):
-        pass
+else:
+    try:
+        datetime.datetime.strptime(requested_date, '%Y-%m-%d')
+    except ValueError:
+        print('Nieprawidłowy format daty. Wprowadź datę we formacie YYYY-mm-dd lub zostaw puste'
+              ' aby sprawdzić jutrzejszy dzień')
+        exit()
+    params = {
+        'latitude' : '52',
+        'longitude' : '16',
+        'start_date' : requested_date,
+        'end_date' : requested_date,
+        'current_weather' : 'true',
+    }
+    url = requests.get('https://api.open-meteo.com/v1/forecast', params = params)
 
-    def __iter__(self):
-        pass
+    get_temperature = url.json()['current_weather']['temperature']
+    get_windspeed = url.json()['current_weather']['windspeed']
+    weather = WeatherForecast(params['latitude'], params['longitude'], requested_date, get_temperature,get_windspeed)
+    history_of_weather[requested_date] = weather.__dict__
+    print(weather)
+    print(history_of_weather)
+    with open(f, 'w') as file:
+        json.dump(history_of_weather, file)
 
-#inquiry = input('Wpisz datę we formacie YY-mm-dd: ')
-params = {
-    'latitude' : '52',
-    'longitude' : '16',
-    'start_date' : '2023-06-25',
-    'end_date' : '2023-06-25',
-    'current_weather' : 'true',
-}
-url = requests.get('https://api.open-meteo.com/v1/forecast', params = params)
 
-get_temperature = url.json()['current_weather']['temperature']
-get_windspeed = url.json()['current_weather']['windspeed']
-print(url.json())
-weather = WeatherForecast(
-    latitude=params['latitude'],
-    longitude = params['longitude'],
-    start_date = params['start_date'],
-    end_date = params['end_date'],
-    temperature = get_temperature,
-    windspeed = get_windspeed
-    )
-print(weather)
 
-with open(f, 'w') as file:
-    json.dump(weather.__dict__, file)
-print(url.status_code)
